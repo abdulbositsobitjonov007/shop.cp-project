76ead9de83a4bd38ca3d1871589f4fbdea7f1c8c
abdulbositsobitjonov007 <sobitjonovabdulbosit91@gmail.com>
2026-02-01 16:32:35 +0500

    adding changes

--- src/components/PhonePanel.jsx
+++ src/components/PhonePanel.jsx
@@ -8,7 +8,7 @@ function PhonePanel() {
         <ul className='flex items-center justify-between'>
               <li className='flex flex-col items-center text-center gap-1'><BiHomeAlt2 className='text-[36px]'/>Main</li>
               <li className='flex flex-col items-center text-center gap-1'><IoSearchSharp className='text-[36px] text-center' />Categories</li>
-              <li className='relative flex flex-col text-center gap-1'><img className='w-9' src="/Frame.svg" alt="" />Cart <span className='absolute top-0.2 -right-1 bg-[red] border border-[black] text-white flex items-center justify-center rounded-[10px] h-[15px] w-[15px]'>0</span></li>
+              <li className='relative flex flex-col text-center gap-1'><img className='w-9' src="/Frame.svg" alt="" />Cart <span className='absolute top-0.2 -right-1 bg-[red] border border-[black] text-white flex items-center justify-center rounded-[10px] h-3.75 w-3.75'>0</span></li>
               <li className='flex flex-col items-center tetx-center gap-1'><img className='w-9' src="/Frame (1).svg" alt="" />Profile</li>
         </ul>
     </div>
